cbafcbfa918f661c4c97e787819061ee3c22fd6a
samin <samin@student.42seoul.kr>
2022-06-08 18:49:50 +0900

    week8

---
+++ src/week08/Student.java
@@ -0,0 +1,15 @@
+package week08;
+
+public class Student {
+    String name;
+    int id;
+    String major;
+    String Mandatory;
+
+    Student(String major)
+    {
+
+    }
+
+    
+}
---
+++ src/week08/TestMain.java
@@ -0,0 +1,7 @@
+package week08;
+
+public class TestMain {
+    public static void main(String[] args) {
+
+    }
+}
